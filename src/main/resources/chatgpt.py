3577f7e1dce5a0a31603049098342c5fa9869af9
rajathy <y.s.rajath@gmail.com>
2023-03-24 19:06:05 +0530

    sdf

--- src/main/resources/chatgpt.py
+++ src/main/resources/chatgpt.py
@@ -9,18 +9,17 @@ def generate_response(prompt, file_path):
 
     response = openai.Completion.create(
         engine="davinci",
-        prompt=prompt,
+        prompt=prompt+file_contents,
         max_tokens=200,
         n=1,
         stop="\n\n",
         temperature=0.5,
-        files={"attachment": file_contents},
     )
 
     message = response.choices[0].text.strip()
     return message
 
-input_message = f"Generate junit for this attached file:\n\n"
+input_message = "Generate junit test case for this java file:\n\n"
 
 fileContent = open('file', 'r')
 Lines = fileContent.readlines()
